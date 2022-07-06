7c6885f02d7daba2912e91513449e55b04e728c7
sysic <sysic@DESKTOP-GAI3575.mshome.net>
2022-07-05 21:05:24 -0500

    Uso de arrays ejemplo profesora ing Emilia Rubio MisionTic Ciclo2 2022 Signed-off-by:sysic <sysic@DESKTOP-GAI3575.mshome.net>

---
+++ src/array1/Array1.java
@@ -0,0 +1,27 @@
+package array1;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        String tienda[] = {"helados", "gomas", "Chocolate", "arequipe"};
+        String[] tienda2 = {"helados2", "gomas2", "Chocolate2", "arequipe2"};
+        double precios[] = {12.5, 5.23, 14.8, 25.4};
+        System.out.println("Lista de productos con precios: ");
+        for (int i = 0; i < tienda.length; i++) {
+            System.out.println(tienda[i] + " = " + precios[i]);
+        }
+        
+        //otra manera d hacer for
+        System.out.println("otro for");
+        var j=0;
+        for (var i : tienda2) {
+
+            System.out.println(i + " == "+precios[j]);
+            j++;
+
+        }
+        
+        System.out.println(((Object)tienda).getClass().getSimpleName());
+        System.out.println(((Object)tienda2).getClass().getSimpleName());
+    }
+}
